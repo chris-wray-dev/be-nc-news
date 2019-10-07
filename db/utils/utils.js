5cdc980487d88ca98b98893f70b6ba6f34d2b418
Chris Wray <chriswray76@gmail.com>
2019-10-07 16:50:45 +0100

    implemented utils functions with testing

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,13 +1,32 @@
 exports.formatDates = list => {
   if (list.length === 0) return [];
   const newList = list.map(object => {
-    const newObject = { ...object };
+    const newObject = {
+      ...object
+    };
     newObject.created_at = new Date(newObject.created_at);
     return newObject;
   });
   return newList;
 };
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = (list, key1, key2) => {
+  if (list.length === 0) return {};
+  if (!key1) key1 = Object.keys(list[0])[1];
+  if (!key2) key2 = Object.keys(list[0])[0];
+  const referenceObject = {};
+  list.forEach(object => {
+    referenceObject[object[key1]] = object[key2];
+  });
+  return referenceObject;
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (list, referenceObject, key1, key2) => {
+  const newList = list.map(object => {
+    const newObject = { ...object };
+    newObject[key2] = referenceObject[object[key1]];
+    delete newObject[key1];
+    return newObject;
+  });
+  return newList;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -69,8 +69,235 @@ describe('formatDates', () => {
     expect(actual).to.eql(expected);
     expect(actual).to.not.equal(input);
   });
+  it('does not mutate the original data', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }, {
+      title: 'Eight pug gifs that remind me of mitch',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'some gifs',
+      created_at: 1289996514171,
+    }];
+    const actual = formatDates(input);
+    const expected = [{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: new Date(1542284514171),
+      votes: 100,
+    }, {
+      title: 'Eight pug gifs that remind me of mitch',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'some gifs',
+      created_at: new Date(1289996514171),
+    }];
+    expect(input).to.eql([{
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }, {
+      title: 'Eight pug gifs that remind me of mitch',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'some gifs',
+      created_at: 1289996514171,
+    }]);
+  });
+
+
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => {
+  it('returns an object when passed an array', () => {
+    expect(makeRefObj([])).to.eql({});
+  });
+  it('returns a reference object when passed an array of one object', () => {
+    const input = [{
+      article_id: 1,
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }];
+    const key1 = 'title';
+    const key2 = 'article_id';
+    const actual = makeRefObj(input, key1, key2);
 
-describe('formatComments', () => {});
+    const expected = {
+      'Living in the shadow of a great man': 1
+    };
+    expect(actual).to.eql(expected);
+  });
+  it('returns a reference object when passed an array of multiple objects', () => {
+    const input = [{
+      article_id: 1,
+      title: 'Living in the shadow of a great man',
+      topic: 'mitch',
+      author: 'butter_bridge',
+      body: 'I find this existence challenging',
+      created_at: 1542284514171,
+      votes: 100,
+    }, {
+      article_id: 2,
+      title: 'Sony Vaio; or, The Laptop',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+      created_at: 1416140514171,
+    }];
+    const key1 = 'title';
+    const key2 = 'article_id';
+    const actual = makeRefObj(input, key1, key2);
+
+    const expected = {
+      'Living in the shadow of a great man': 1,
+      'Sony Vaio; or, The Laptop': 2
+    };
+    expect(actual).to.eql(expected);
+  });
+});
+it('does not mutate the original data', () => {
+  const input = [{
+    article_id: 1,
+    title: 'Living in the shadow of a great man',
+    topic: 'mitch',
+    author: 'butter_bridge',
+    body: 'I find this existence challenging',
+    created_at: 1542284514171,
+    votes: 100,
+  }, {
+    article_id: 2,
+    title: 'Sony Vaio; or, The Laptop',
+    topic: 'mitch',
+    author: 'icellusedkars',
+    body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+    created_at: 1416140514171,
+  }];
+  const key1 = 'title';
+  const key2 = 'article_id';
+  const actual = makeRefObj(input, key1, key2);
+
+  const expected = {
+    'Living in the shadow of a great man': 1,
+    'Sony Vaio; or, The Laptop': 2
+  };
+  expect(input).to.eql([{
+    article_id: 1,
+    title: 'Living in the shadow of a great man',
+    topic: 'mitch',
+    author: 'butter_bridge',
+    body: 'I find this existence challenging',
+    created_at: 1542284514171,
+    votes: 100,
+  }, {
+    article_id: 2,
+    title: 'Sony Vaio; or, The Laptop',
+    topic: 'mitch',
+    author: 'icellusedkars',
+    body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+    created_at: 1416140514171,
+  }]);
+});
+
+describe('formatComments', () => {
+  it('returns a new array when passed an array', () => {
+    const input = [];
+    expect(formatComments(input)).to.eql([]);
+    expect(formatComments(input)).to.not.equal(input);
+  });
+  it('returns an array with formatted object when passed an array of one object and a reference object', () => {
+    const input = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }];
+    const referenceObj = {
+      "They're not exactly dogs, are they?": 1
+    }
+    const actual = formatComments(input, referenceObj, 'belongs_to', 'article_id');
+    const expected = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      article_id: 1,
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }];
+    expect(actual).to.eql(expected);
+  });
+  it('returns an array with formatted object when passed an array of multiple objects and a reference object', () => {
+    const input = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }, {
+      body: 'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+      belongs_to: 'Living in the shadow of a great man',
+      created_by: 'butter_bridge',
+      votes: 14,
+      created_at: 1479818163389,
+    }];
+    const referenceObj = {
+      "They're not exactly dogs, are they?": 1,
+      "Living in the shadow of a great man": 2
+    }
+    const actual = formatComments(input, referenceObj, 'belongs_to', 'article_id');
+    const expected = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      article_id: 1,
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }, {
+      body: 'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+      article_id: 2,
+      created_by: 'butter_bridge',
+      votes: 14,
+      created_at: 1479818163389,
+    }];
+    expect(actual).to.eql(expected);
+  });
+  it('does not mutate the original data', () => {
+    const input = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }];
+    const referenceObj = {
+      "They're not exactly dogs, are they?": 1
+    }
+    const actual = formatComments(input, referenceObj, 'belongs_to', 'article_id');
+    const expected = [{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      article_id: 1,
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }];
+    expect(input).to.eql([{
+      body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    }]);
+  });
+});
